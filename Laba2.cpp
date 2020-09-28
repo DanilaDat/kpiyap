1d28bf9a9fc016a3ab32e9f190c4867c87bf4278
Polyushka <70941524+Polyushka@users.noreply.github.com>
2020-09-28 22:03:25 +0300

    Update Laba2.cpp

--- Laba2.cpp
+++ Laba2.cpp
@@ -22,12 +22,36 @@ public:
 	~automobile() {};
 	friend void print(automobile &avto);
 	friend class STO;
+	friend void input_id(automobile &avt);
+	friend void set_id(int ID, string fio, automobile &avto) {
+		avto.id = ID;
+		avto.name = fio;
+	};
 };
 void print(automobile &avto) {
-	cout << "Уникальный номер автомобиля  - " << avto.id << endl;
+	cout << "Уникальный номер автомобиля - " << avto.id << endl;
 	cout << "ФИО владельца автомобиля - " << avto.name << endl;
 	cout << "Пробег автомобиля - " << avto.mileage << endl;
 }
+void input_id(automobile &avto) {
+int ID;
+	string fio;
+	bool a = true;
+	do
+	{
+		cout << "Введите id: ";
+		cin >> ID;
+		if (cin.fail())
+			cout << "Повторите попытку" << endl;
+		else
+			a = false;
+		cin.clear();
+		cin.ignore(cin.rdbuf()->in_avail());
+	} while (a);
+	cout << "Введите ФИО владельца автомобиля - ";
+	cin >> fio;
+	set_id(ID, fio,avto);
+};
 class STO {
 private:
 	int Mileage;
@@ -58,31 +82,48 @@ public:
 		avto.mileage = Mileage;
 	}
 };
+
 int main() {
 	system("chcp 1251>null");
-	int ID;
-	string fio;
-	bool a = true;
-	do
-	{
-		cout << "Введите id: ";
-		cin >> ID;
-		if (cin.fail())
-			cout << "Повторите попытку" << endl;
-		else
-			a = false;
-		cin.clear();
-		cin.ignore(cin.rdbuf()->in_avail());
-	} while (a);
-	cout << "Введите ФИО владельца автомобиля - ";
-	cin >> fio;
-	automobile a1(ID, fio);
 	STO m1;
-	print(a1);
+	automobile Avto[10];
+	int i = 0,answ=0,count=0;
+	do {
+		cout << "Добавить автомобиль в базу?(Да-1)" << endl;
+		int answer = 0;
+		answ = 0;
+		cin >> answer;
+		if (answer == 1)
+		{input_id(Avto[i]);
+		cout << "========================================" << endl;
+		print(Avto[i]);
+		cout << "========================================" << endl;
+		m1.input_mileage();
+		m1.install_mileage(Avto[i]);
+		cout << "========================================" << endl;
+		print(Avto[i]);
+		i++;
+		count++;
+	}
 	cout << "========================================" << endl;
-	m1.input_mileage();
-	m1.install_mileage(a1);
-	cout << "========================================" << endl;
-	print(a1);
+	cout << "Хотите прродолжить работу? (Да-1)" << endl;
+	cin.clear();
+	cin.ignore(cin.rdbuf()->in_avail());
+		cin >> answ;
+	} while (answ == 1 && i != 10);
+	cout << "Хотите вывести информацию о всех автомобилях? ";
+	cin.clear();
+	cin.ignore(cin.rdbuf()->in_avail());
+	int answ1;
+	cin >> answ1;
+	if (answ1 == 1) {
+		system("CLS");
+		for (int j = 0; j < count; j++)
+		{
+			cout << "Автомобиль № " << j + 1 << endl;
+			print(Avto[j]);
+			cout << "========================================" << endl;
+		}
+	}
 	return 0;
 }
